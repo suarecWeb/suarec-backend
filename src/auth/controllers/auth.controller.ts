4533d2f6376d066840c2748423a28a8087b667e8
dylanbc1 <dyez1110@gmail.com>
2025-03-05 17:03:57 -0500

    feat: Change login token

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Res, Param } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { Response } from 'express'; // Importa Response de express
 import { passwordDto } from '../dto/password.dto';
 import { Public } from '../decorators/public.decorator';
 
@@ -7,10 +8,19 @@ import { Public } from '../decorators/public.decorator';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Public()
   @Post('login')
-  create(@Body() signInDto: Record<string, string>) {
-    return this.authService.signIn(signInDto.email, signInDto.password);
+  async login(@Body() signInDto: { email: string; password: string }, @Res() res: Response) {
+    const token = await this.authService.signIn(signInDto.email, signInDto.password);
+
+    // Usa res.cookie() correctamente
+    res.cookie('auth_token', token, {
+      httpOnly: true, // Protege contra XSS
+      secure: false, // Solo en HTTPS (desactivar en desarrollo si es necesario)
+      sameSite: 'strict', // Protege contra CSRF
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 semana
+    });
+
+    return res.send({ message: 'Login exitoso' });
   }
 
   @Public()
