c6ee8000db79759e12f1e82c681daddef9665147
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-05-03 13:08:09 -0500

    fix: update cors

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     app.use(cookieParser()); // Permitir leer cookies
 
     const corsOptions: CorsOptions = {
-      origin: ['http://localhost:3000', process.env.PUBLIC_FRONT_URL], // ✅ Especifica el origen permitido
+      origin: ['http://localhost:3000', process.env.PUBLIC_FRONT_URL, 'https://suarec-frontend-production.up.railway.app', 'https://suarec.com'], // ✅ Especifica el origen permitido
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
       allowedHeaders: 'Content-Type, Accept, Authorization',
