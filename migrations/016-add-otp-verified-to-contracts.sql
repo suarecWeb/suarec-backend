827647645ed0a4d95cdd21e64e35f9372acefa24
Danielms111 <103274916+Danielms111@users.noreply.github.com>
2025-08-28 19:34:52 -0500

    fix: fixing otp verification modal and some other issues related to contracts status

--- migrations/016-add-otp-verified-to-contracts.sql
+++ migrations/016-add-otp-verified-to-contracts.sql
@@ -6,3 +6,5 @@ CREATE INDEX idx_contract_otp_verified ON contract(otp_verified);
 
 -- Update existing completed contracts to have otpVerified = false
 UPDATE contract SET otp_verified = FALSE WHERE status = 'completed';
+
+
